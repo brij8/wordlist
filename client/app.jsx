0109f3abfd8911a4ad3c7bb77c1929dbf978da1b
brij <bmuses@gmail.com>
2022-04-17 13:31:40 -0700

    F1 progress

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import Home from './pages/home';
+import Nav from './components/nav';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      route: parseRoute(location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      const route = parseRoute(location.hash);
+      this.setState({ route });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+
+    if (route.path === '') {
+      return <Home />;
+    }
+
+    return (
+      <div className="py-5">
+        <h1 className='text-center text-danger'>404 - Page Not Found</h1>
+      </div>
+    );
+  }
+
   render() {
-    return <Home />;
+    return (
+      <div>
+        <Nav/>
+        { this.renderPage() }
+      </div>
+    );
   }
 }
---
+++ client/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Nav() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className = "navbar-brand" href="#">Wordlist</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
