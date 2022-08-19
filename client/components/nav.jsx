a745e2787fe1dbf89f5956d9c4ec8e411abfd9c0
brij <bmuses@gmail.com>
2022-08-18 18:14:26 -0700

    change nav links from collumn to row

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -4,8 +4,7 @@ function Nav() {
     <nav className="navbar navbar-dark bg-dark">
       <div className="container-fluid">
         <a className = "navbar-brand" href="#home">Wordlist</a>
-      </div>
-      <div className="nav-text">
+
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <a className="nav-link" href="#account">Account</a>
@@ -17,8 +16,8 @@ function Nav() {
             <a className="nav-link" href="#lists">Lists</a>
           </li>
         </ul>
-      </div>
 
+    </div>
     </nav>
   );
 }
